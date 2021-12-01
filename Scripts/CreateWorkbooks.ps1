c6a6d8686a380ec5e59100cfdeea19c4139ea8c3
bin-ini <60436133+bin-ini@users.noreply.github.com>
2021-12-01 15:32:43 +0100

    Update CreateWorkbooks.ps1

--- Scripts/CreateWorkbooks.ps1
+++ Scripts/CreateWorkbooks.ps1
@@ -4,14 +4,15 @@ param(
     [Parameter(Mandatory=$true)]$WorkbooksFolder,
     [Parameter(Mandatory=$true)]$Workspace
 )
+echo "WorkbooksFolder 1.2- ${WorkbooksFolder}"
+echo "Workspace 1.2- ${Workspace}"
 
 Write-Host "Folder is: $($WorkbooksFolder)"
 
 $armTemplateFiles = Get-ChildItem -Path $WorkbooksFolder -Filter *.json
 
 echo "Reached 1.2- ${armTemplateFiles}"
-echo "WorkbooksFolder 1.2- ${WorkbooksFolder}"
-echo "Workspace 1.2- ${Workspace}"
+
 
 
 Write-Host "Files are: " $armTemplateFiles
