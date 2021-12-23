9d3df3ad65ffc77f7575f5933d24c8de9cabd36c
bin-ini <60436133+bin-ini@users.noreply.github.com>
2021-12-23 18:14:39 +0100

    Create connect.ps1

---
+++ Scripts/connect.ps1
@@ -0,0 +1,21 @@
+param(
+    [Parameter(Mandatory=$true)]$applicationId,
+    [Parameter(Mandatory=$true)]$tenantId,   
+    [Parameter(Mandatory=$true)]$clientSecret  
+)
+echo "Reached 1- ${aplicationId}"
+echo "Reached 2- ${tenantId}"
+echo "Reached 3- ${clientSecret}"
+
+
+try {
+    $azurePassword = ConvertTo-SecureString $clientSecret -AsPlainText -Force
+    $psCred = New-Object System.Management.Automation.PSCredential($applicationId , $azurePassword)
+    Connect-AzAccount -Credential $psCred -TenantId $tenantId  -ServicePrincipal 
+}
+catch {
+    echo "Reached 5"
+    $ErrorMessage = $_.Exception.Message
+    echo "Reached 6 - $ErrorMessage"
+    Write-Error "Rule import failed with message: $ErrorMessage" 
+}
