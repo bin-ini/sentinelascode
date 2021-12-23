94b5d2bbed48c462d884d66098127d0873d92efc
bin-ini <60436133+bin-ini@users.noreply.github.com>
2021-12-23 23:10:36 +0100

    Update connectAzure.ps1

--- Scripts/connectAzure.ps1
+++ Scripts/connectAzure.ps1
@@ -1,14 +1,11 @@
-
-echo "Reached 1- ${env.CLIENT_ID}"
-echo "Reached 2- ${env.TENANT_ID}"
-echo "Reached 2- ${env.CLIENT_SECRET}"
-echo "Reached 3- ${env.SUBS_ID}"
-
+param(
+    [Parameter(Mandatory=$true)]$Credentials
+)
 
 try {
-    $azurePassword = ConvertTo-SecureString ${env.CLIENT_SECRET} -AsPlainText -Force
-    $psCred = New-Object System.Management.Automation.PSCredential(${env.CLIENT_ID} , $azurePassword)
-    Connect-AzAccount -Credential $psCred -TenantId ${env.TENANT_ID} -Subscription ${env.SUBS_ID} -ServicePrincipal 
+    $azurePassword = ConvertTo-SecureString ${Credentials.CLIENT_SECRET} -AsPlainText -Force
+    $psCred = New-Object System.Management.Automation.PSCredential(${Credentials.CLIENT_ID} , $azurePassword)
+    Connect-AzAccount -Credential $psCred -TenantId ${Credentials.TENANT_ID} -Subscription ${Credentials.SUBS_ID} -ServicePrincipal 
 }
 catch {
     echo "Reached 5"
