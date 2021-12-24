2fbffbe053fbea8c6d55d239e55c83df3bcd3da8
bin-ini <60436133+bin-ini@users.noreply.github.com>
2021-12-24 08:11:08 +0100

    Update connectAzure.ps1

--- Scripts/connectAzure.ps1
+++ Scripts/connectAzure.ps1
@@ -1,10 +1,10 @@
 echo "SUBS_ID - ${env:SUBS_ID}, CLIENT_ID - ${env:CLIENT_ID}"
 
 try {
+    Disable-AzContextAutosave
     $azurePassword = ConvertTo-SecureString ${env:CLIENT_SECRET} -AsPlainText -Force
     $psCred = New-Object System.Management.Automation.PSCredential(${env:CLIENT_ID} , $azurePassword)
     Connect-AzAccount -Credential $psCred -TenantId ${env:TENANT_ID} -Subscription ${env:SUBS_ID} -ServicePrincipal
-    Disable-AzContextAutosave -Scope Process
 }
 catch {
     echo "Reached 5"
