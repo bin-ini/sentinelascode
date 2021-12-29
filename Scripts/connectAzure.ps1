d29ead853584bf0df73fc7282302259af170888d
bin-ini <60436133+bin-ini@users.noreply.github.com>
2021-12-29 12:03:43 +0100

    Update connectAzure.ps1

--- Scripts/connectAzure.ps1
+++ Scripts/connectAzure.ps1
@@ -1,11 +1,9 @@
 try {
-    $azurePassword = ConvertTo-SecureString ${env:CLIENT_SECRET} -AsPlainText -Force
-    $psCred = New-Object System.Management.Automation.PSCredential(${env:CLIENT_ID} , $azurePassword)
-    Connect-AzAccount -Credential $psCred -TenantId ${env:TENANT_ID} -Subscription ${env:SUBS_ID} -ServicePrincipal
+    $azurePassword = ConvertTo-SecureString ${env:ClientSecret} -AsPlainText -Force
+    $psCred = New-Object System.Management.Automation.PSCredential(${env:ClientId} , $azurePassword)
+    Connect-AzAccount -Credential $psCred -TenantId ${env:TenantId} -Subscription ${env:SubscriptionId} -ServicePrincipal
 }
 catch {
-    echo "Reached 5"
     $ErrorMessage = $_.Exception.Message
-    echo "Reached 6 - $ErrorMessage"
     Write-Error "Connection to Azure failed with message: $ErrorMessage" 
 }
