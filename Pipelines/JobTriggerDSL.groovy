2c97921fb9d13cf42ef5e21aed69a56fae8f33fc
bin-ini <60436133+bin-ini@users.noreply.github.com>
2022-01-11 17:54:39 +0100

    Create JobTriggerDSL.groovy

---
+++ Pipelines/JobTriggerDSL.groovy
@@ -0,0 +1,26 @@
+pipelineJob('SentinelCICDRelevantJob') {
+
+  def repo = 'https://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@github.com/bin-ini/sentinelascode.git'
+
+  description("Pipeline for $repo")
+  
+  parameters {
+        stringParam('jenkinsFileSelected', '...', '...')
+    }
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master')
+          scriptPath("${jenkinsFileSelected}")
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+
+      }
+    }
+  }
+  
+}
+  queue('SentinelCICDRelevantJob')
